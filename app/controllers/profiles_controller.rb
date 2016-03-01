8434cd6d87665fcce73ecbfc167a6c58e91398ce
Albert Chen <algebraprodigy@gmail.com>
2016-03-01 15:28:56 +0800

    added authentication to profile edits, also added linking

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,15 +1,16 @@
 class ProfilesController < ApplicationController
+	before_action :set_user
+	before_action :owned_profile, only: [:edit, :update]
+	before_action :authenticate_user!
+
   def show
-  	@user = User.find_by(user_name: params[:user_name])
   	@posts = User.find_by(user_name: params[:user_name]).posts.order('created_at DESC')
   end
 
   def edit
-  	@user = User.find_by(user_name: params[:user_name])
   end
 
   def update
-  	@user = User.find_by(user_name: params[:user_name])
   	if @user.update(profile_params)
   		flash[:success] = 'Profile Updated'
   		redirect_to profile_path(@user.user_name)
@@ -23,5 +24,16 @@ class ProfilesController < ApplicationController
   private
   def profile_params
   	params.require(:user).permit(:avatar, :bio)
+  end
+
+  def owned_profile
+  	unless current_user == @user
+  		flash[:alert] = "You cannot edit this profile"
+  		redirect_to root_path
+  	end
+  end
+
+  def set_user
+  	@user = User.find_by(user_name: params[:user_name])
   end
 end
